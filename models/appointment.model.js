720ceffed6c92529e2b863a0ec70575dfe7fd45b
marwa <marwa.rekik@daveo.fr>
2021-05-10 04:45:30 +0200

    prestation from db

--- models/appointment.model.js
+++ models/appointment.model.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 ObjectId = mongoose.Schema.Types.ObjectId;
 
 const Appointment = mongoose.model(
-   "Appointment",
-  new mongoose.Schema({ 
-   user: { type: ObjectId, ref: 'User', required: true },
-   prestation: {type: String, required: true },
-   created_at: Date,
-   slots: { type: ObjectId, ref: 'Slot' },
-})
-)
+  "Appointment",
+  new mongoose.Schema({
+    user: { type: ObjectId, ref: "User", required: true },
+    prestation: { type: String, required: true },
+    created_at: Date,
+    slots: { type: ObjectId, ref: "Slot" },
+  })
+);
 
-module.exports = Appointment
+module.exports = Appointment;
