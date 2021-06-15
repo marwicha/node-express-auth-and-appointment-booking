66bf1dabfb53dc9ebd172f3c3c54e46ad8493eeb
marwa <marwa.rekik@daveo.fr>
2021-06-15 17:34:33 +0200

    mail options

--- utils/sendEmails.js
+++ utils/sendEmails.js
@@ -5,31 +5,27 @@ const path = require("path");
 
 const sendEmail = async (email, subject, payload, template) => {
   try {
-    let testAccount = await nodemailer.createTestAccount();
     // create reusable transporter object using the default SMTP transport
     const transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false,
+      host: "smtp.mailtrap.io",
+      port: 2525,
       auth: {
-        user: testAccount.user,
-        pass: testAccount.pass, // naturally, replace both with your real credentials or an application-specific password
+        user: "1a2b3c4d5e6f7g",
+        pass: "1a2b3c4d5e6f7g",
       },
     });
 
     const source = fs.readFileSync(path.join(__dirname, template), "utf8");
     const compiledTemplate = handlebars.compile(source);
-    const options = () => {
-      return {
-        from: "marwa.rekik.pro@gmail.com",
-        to: email,
-        subject: subject,
-        html: compiledTemplate(payload),
-      };
+    const options = {
+      from: "marwa.rekik.pro@gmail.com",
+      to: email,
+      subject: subject,
+      html: compiledTemplate(payload),
     };
 
     // Send email
-    transporter.sendMail(options(), (error, info) => {
+    transporter.sendMail(options, (error, info) => {
       if (error) {
         return error;
       } else {
