1cd63a58c4e514a4745b5b0e522b4a9d6acdc7cf
marwa <marwa.rekik@daveo.fr>
2021-06-24 12:11:30 +0200

    config secret

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -97,7 +97,9 @@ exports.signin = (req, res) => {
         });
       }
 
-      const token = jwt.sign({ id: user.id }, JWTSecret);
+      const token = jwt.sign({ id: user.id }, config.secret, {
+        expiresIn: 86400, // 24 hours
+      });
 
       const authorities = [];
 
