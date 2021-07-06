bf245c41c3895d599621c82136dfc45d35f91523
marwa <marwa.rekik@daveo.fr>
2021-07-06 22:39:25 +0200

    revert

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -2,5 +2,9 @@ const controller = require("../controllers/admin.controller");
 const { authJwt } = require("../middlewares");
 
 module.exports = function (app) {
-  app.get("/api/getAll", [authJwt.isAdmin], controller.allAppointmentsAdmin);
+  app.get(
+    "/api/getAll",
+    [authJwt.verifyToken],
+    controller.allAppointmentsAdmin
+  );
 };
