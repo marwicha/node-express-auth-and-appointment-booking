acfe7b668532d72cff544ffbba17826fbced9e40
marwa <marwa.rekik@daveo.fr>
2021-07-06 22:37:30 +0200

    is admin in route

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -2,9 +2,5 @@ const controller = require("../controllers/admin.controller");
 const { authJwt } = require("../middlewares");
 
 module.exports = function (app) {
-  app.get(
-    "/api/getAll",
-    [authJwt.verifyToken],
-    controller.allAppointmentsAdmin
-  );
+  app.get("/api/getAll", [authJwt.isAdmin], controller.allAppointmentsAdmin);
 };
