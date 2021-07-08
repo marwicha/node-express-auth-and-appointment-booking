91005213784e5fce6a19888a9bdeba2d9f99474d
marwa <marwa.rekik@daveo.fr>
2021-07-08 14:51:19 +0200

    change route formation

--- routes/formation.routes.js
+++ routes/formation.routes.js
@@ -3,25 +3,19 @@ const { authJwt } = require("../middlewares");
 
 module.exports = function (app) {
   app.post(
-    "/api/formation/create",
+    "/api/admin/formation/create",
     [authJwt.verifyToken],
     controller.addFormation
   );
 
   app.get(
-    "/api/formation/all",
+    "/api/admin/formation/all",
     [authJwt.verifyToken],
     controller.allFormations
   );
 
-  app.put(
-    "/api/formation/update/:id",
-    [authJwt.verifyToken],
-    controller.updateFormation
-  );
-
   app.delete(
-    "/api/formation/delete/:id",
+    "/api/admin/formation/delete/:id",
     [authJwt.verifyToken],
     controller.delete
   );
